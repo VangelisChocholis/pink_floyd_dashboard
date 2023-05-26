1a2dea9796202abff09dd1094d6b3fa7fc24adc4
VangelisChocholis <105212289+VangelisChocholis@users.noreply.github.com>
2023-05-26 22:51:24 +0300

    add descriptions

--- pink_floyd_streamlit_app.py
+++ pink_floyd_streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+from descriptions import descr_dict 
 
 df = pd.read_csv('pink_floyd_tracks_updated.csv', parse_dates=[5])
 
