e7ccca948ae6c9376f1c489b5d29373a3cdb3cb9
Evangelos Chocholis <105212289+VangelisChocholis@users.noreply.github.com>
2023-06-01 19:15:59 +0300

    adjustments

--- update_pink_floyd_popularity_data.py
+++ update_pink_floyd_popularity_data.py
@@ -1,43 +1,11 @@
 import pandas as pd
+from datetime import date
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # read Pink Floyd data 
 # check get_pink_floyd_data_to_csv.py to see how we got them from a databse
 df = pd.read_csv('pink_floyd_tracks.csv')
-
-# convert to datetime, release_date is in Unix format (in ms)
-df['release_date'] = pd.to_datetime(df['release_date'], unit='ms')
-
-# main Pink Floyd albums
-album_num_tracks = {
-    'The Piper at the Gates of Dawn': 11,
-    'A Saucerful of Secrets': 7,
-    'More': 13,
-    'Ummagumma': 12,
-    'Atom Heart Mother': 5,
-    'Meddle': 6,
-    'Obscured by Clouds': 10,
-    'The Dark Side of the Moon': 10,
-    'Wish You Were Here': 5,
-    'Animals': 5,
-    'The Wall': 26,
-    'The Final Cut': 13,
-    'A Momentary Lapse of Reason': 11,
-    'The Division Bell': 11
-}
-
-# filter df to inlude tracks from main albums
-df = df[df['album_name'].isin(album_num_tracks.keys())]
-
-# make sure to drop duplicates
-df = df[~df['track_id'].duplicated()]
-
-# remove remastered versions
-df = df[~df['track_name'].str.contains('remaster', case=False)]
-
-# remove live versions
-df = df[~df['track_name'].str.contains('- live', case=False)]
 
 # acces spotipy 
 from spotify_config import config
@@ -60,17 +28,26 @@ alb_pop_df = pd.DataFrame(data={'album_id': alb_pop.keys(),
 # merge to pass album popularity information
 df = pd.merge(df, alb_pop_df, on='album_id')
 
-# get current Pink Floyd popularity
-pink_floyd_popularity = sp.artist(df['artist_id'][0])['popularity']
-#print(f'Current Pink Floyd popularity: {pink_floyd_popularity}')
-
 # check number of tracks and save to csv
 tot_tracks = sum(album_num_tracks.values())
 if tot_tracks == df.shape[0]:
-    print('Great! We have collected all the tracks')
     df.to_csv('pink_floyd_tracks_updated.csv', index=False)
 else:
-    print('Error! Unable to collect the correct number of tracks')
+    raise Exception('Error! Unable to collect the correct number of tracks')
 
-    
+
+# get current Pink Floyd popularity
+pink_floyd_popularity = sp.artist(df['artist_id'][0])['popularity']
+# read popularity by date csv
+pink_floyd_art_pop = pd.read_csv('pink_floyd_artist_popularity.csv')
+current_date = date.today()
+pop_dict = {'date': [current_date],
+          'artist_popularity': [pink_floyd_popularity]}+
+# make new df
+pink_floyd_art_pop_new = pd.DataFrame(pop_dict)
+# concat old and new 
+p = pd.concat((pink_floyd_art_pop, pink_floyd_art_pop_new), axis=0)
+# replace csv with new data
+p.to_csv('pink_floyd_artist_popularity.csv', index=False)
+
 
