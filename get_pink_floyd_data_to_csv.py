5df265bf6f0b24cda2478debe1ce4ed70f2dea3a
Evangelos Chocholis <105212289+VangelisChocholis@users.noreply.github.com>
2023-06-01 19:14:33 +0300

    adjustments

--- get_pink_floyd_data_to_csv.py
+++ get_pink_floyd_data_to_csv.py
@@ -23,9 +23,42 @@ conn.close()
 
 # remove duplicates
 df = df[~df['track_id'].duplicated()]
+
 # remove columns (we already have duration)
 df = df.drop(['track_duration', 'analysis_url'], axis=1)
+
 # convert duration to minutes
 df['duration'] = df['duration']/60000
+
+# convert to datetime, release_date is in Unix format (in ms)
+df['release_date'] = pd.to_datetime(df['release_date'], unit='ms')
+
+# main Pink Floyd albums
+album_num_tracks = {
+    'The Piper at the Gates of Dawn': 11,
+    'A Saucerful of Secrets': 7,
+    'More': 13,
+    'Ummagumma': 12,
+    'Atom Heart Mother': 5,
+    'Meddle': 6,
+    'Obscured by Clouds': 10,
+    'The Dark Side of the Moon': 10,
+    'Wish You Were Here': 5,
+    'Animals': 5,
+    'The Wall': 26,
+    'The Final Cut': 13,
+    'A Momentary Lapse of Reason': 11,
+    'The Division Bell': 11
+}
+
+# filter df to inlude tracks from main albums
+df = df[df['album_name'].isin(album_num_tracks.keys())]
+
+# remove remastered versions
+df = df[~df['track_name'].str.contains('remaster', case=False)]
+
+# remove live versions
+df = df[~df['track_name'].str.contains('- live', case=False)]
+
 # save to csv
 df.to_csv('pink_floyd_tracks.csv', index=False)
